5c613b88c52b7f8fb5a22c171003aee28cd3a203
Shorn Jacob <shorn@transparency.consulting>
2020-02-19 13:52:12 +1300

    Added a environmnet varaible

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AWS from "aws-sdk";
+
+AWS.config.region = "ap-southeast-2"; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: process.env.REACT_APP_COGNITO_IDENTITYPOOL_ID
+});
 
 function App() {
+
+  console.log(process.env);
+  
   return (
     <div className="App">
       <header className="App-header">
