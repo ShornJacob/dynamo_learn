c13a17ca03ae967751f3e29e67f3463bbcabac00
Shorn Jacob <shorn@transparency.consulting>
2020-02-20 13:04:18 +1300

    create table with form

--- src/CreateTable.js
+++ src/CreateTable.js
@@ -1,15 +1,98 @@
 import React from "react";
 import { useForm } from 'react-hook-form'
+import AWS from "aws-sdk";
+
+AWS.config.region = "ap-southeast-2"; // Region
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: process.env.REACT_APP_COGNITO_IDENTITYPOOL_ID
+});
+
 
 //https://docs.amazonaws.cn/en_us/amazondynamodb/latest/APIReference/API_AttributeValue.html
 //https://docs.aws.amazon.com/cli/latest/reference/dynamodb/create-table.html
+//https://docs.aws.amazon.com/amazondynamodb/latest/developerguide/WorkingWithTables.Basics.html#WorkingWithTables.Basics.CreateTable
+
+function createParams(data) {
+  return   {
+    AttributeDefinitions: [
+      {
+        AttributeName: data.attrid1,
+        AttributeType: data.attrname1
+      },
+      {
+        AttributeName: data.attrid2,
+        AttributeType: data.attrname2
+      }
+    ],
+    // https://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/aws-properties-dynamodb-keyschema.html
+    //https://stackoverflow.com/questions/30866030/number-of-attributes-in-key-schema-must-match-the-number-of-attributes-defined-i
+    //An array of attributes that describe the key schema for the table and indexes.
+
+    KeySchema: [
+      {
+        AttributeName: data.attrid1,
+        KeyType: "HASH"
+      },
+      {
+        AttributeName: data.attrid2,
+        KeyType: "RANGE"
+      }
+    ],
+    //https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_ProvisionedThroughput.html
+    //The maximum number of strongly consistent reads consumed per second before DynamoDB returns a ThrottlingException
+    //he maximum number of writes consumed per second before DynamoDB returns a ThrottlingException.
+    ProvisionedThroughput: {
+      ReadCapacityUnits: 1,
+      WriteCapacityUnits: 1
+    },
+   
+    TableName: data.tablename,
+
+    //https://docs.aws.amazon.com/amazondynamodb/latest/APIReference/API_StreamSpecification.html
+    //A DynamoDB stream is an ordered flow of information about changes to items in a DynamoDB table. When you enable a stream on a table, DynamoDB captures information about every modification to data items in the table.
+    StreamSpecification: {
+      StreamEnabled: false
+    }
+  }
+}
+
+function createTableinDynamo(params) {
+ 
+
+  console.log(process.env)
+
+  AWS.config.getCredentials(function(err) {
+    if (err) console.log(err.stack)
+    else {
+      const ddb = new AWS.DynamoDB({ apiVersion: "2012-08-10" });
+      ddb.createTable(params, function(err, data) {
+        if (err) {
+          console.log("Error", err);
+        } else {
+          console.log("Table Created", data);
+        }
+      })
+    }
+  })
+
+
+}
 
 
 export default function() {
 
   const { register, handleSubmit } = useForm()
-  const onSubmit = data => { console.log(data) }
 
+   function onSubmit(data) { 
+    console.log(data)
+    const params = createParams(data)
+    console.log(params)
+    createTableinDynamo(params)
+
+   }
+
+  // Create the DynamoDB service object
+ 
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
@@ -19,9 +102,9 @@ export default function() {
        <label htmlFor="attrid1">Attribute ID: </label>
        <input type="text" id="attrid1" name="attrid1" ref={register({ required: true })}/>
 
-       <label htmlFor="attrtype1">Type</label>
+       <label htmlFor="attrname1">Type</label>
 
-       <select id="attrtype1" name="attrtype1" ref={register}>
+       <select id="attrname1" name="attrname1" ref={register}>
           <option value="N">Number</option>
            <option value="S">String</option>
         </select>
@@ -31,9 +114,9 @@ export default function() {
         <label htmlFor="attrid2">Attribute ID: </label>
        <input type="text" id="attrid2" name="attrid2" ref={register({ required: true })}/>
 
-       <label fhtmlFor="attrtype2">Type</label>
+       <label htmlFor="attrname2">Type</label>
 
-       <select id="attrtype2" name="attrtype2" ref={register}>
+       <select id="attrname2" name="attrname2" ref={register}>
           <option value="N">Number</option>
            <option value="S">String</option>
         </select>
