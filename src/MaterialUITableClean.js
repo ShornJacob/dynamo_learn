b13139bc69ee8795a9aca8e2933e77ec131dec73
shaun jacob <sjacobnz86@gmail.com>
2020-10-02 02:15:46 +1300

    working muitable

--- src/MaterialUITableClean.js
+++ src/MaterialUITableClean.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -11,63 +10,44 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+const StyledTableCell = withStyles((theme) => ({
+    head: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    body: {
+      fontSize: 14,
+    },
+  }))(TableCell);
+
+function createData(id,time,user) {
+    return { id, time, user };
 }
 
 const rows = [
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Donut', 452, 25.0, 51, 4.9),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
-    createData('Honeycomb', 408, 3.2, 87, 6.5),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Jelly Bean', 375, 0.0, 94, 0.0),
-    createData('KitKat', 518, 26.0, 65, 7.0),
-    createData('Lollipop', 392, 0.2, 98, 0.0),
-    createData('Marshmallow', 318, 0, 81, 2.0),
-    createData('Nougat', 360, 19.0, 9, 37.0),
-    createData('Oreo', 437, 18.0, 63, 4.0),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com'),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com'),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com'),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com'),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com'),
+    createData('2009060155168', '06-09-20 01:55', 'sht@der.com')
 ];
 
 
-const headCells = [
-    { id: 'name', numeric: false, disablePadding: true, label: 'Dessert (100g serving)' },
-    { id: 'calories', numeric: true, disablePadding: false, label: 'Calories' },
-    { id: 'fat', numeric: true, disablePadding: false, label: 'Fat (g)' },
-    { id: 'carbs', numeric: true, disablePadding: false, label: 'Carbs (g)' },
-    { id: 'protein', numeric: true, disablePadding: false, label: 'Protein (g)' },
-];
-
-function EnhancedTableHead(props) {
-
+function MUITableHead() {
     return (
-
         <TableHead>
-            <TableRow>
-                {headCells.map((headCell) => (
-                    <TableCell
-                        key={headCell.id}
-                        align={headCell.numeric ? 'right' : 'left'}
-                        padding={headCell.disablePadding ? 'none' : 'default'}
-                    >
-                        {headCell.label}
-                    </TableCell>
-                ))
-
-                }
-
+            <TableRow>          
+                     <StyledTableCell>ID</StyledTableCell>
+                     <StyledTableCell>Initial Request Time</StyledTableCell>
+                     <StyledTableCell>User</StyledTableCell>
             </TableRow>
-
         </TableHead>
     );
 }
 
-EnhancedTableHead.propTypes = {
-    classes: PropTypes.object.isRequired,
-    rowCount: PropTypes.number.isRequired,
-};
+
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -79,22 +59,11 @@ const useStyles = makeStyles((theme) => ({
         marginBottom: theme.spacing(2),
     },
     table: {
-        minWidth: 750,
-    },
-    visuallyHidden: {
-        border: 0,
-        clip: 'rect(0 0 0 0)',
-        height: 1,
-        margin: -1,
-        overflow: 'hidden',
-        padding: 0,
-        position: 'absolute',
-        top: 20,
-        width: 1,
-    },
+        minWidth: 450,
+    }
 }));
 
-export default function EnhancedTable() {
+export default function MUITable() {
     const classes = useStyles();
 
     const [page, setPage] = React.useState(0);
@@ -103,15 +72,12 @@ export default function EnhancedTable() {
     const rowsPerPage = 5
 
     const handleClick = (event, name) => {
-
         console.log(name)
-
     };
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
     };
-
 
     const emptyRows = rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
 
@@ -123,45 +89,46 @@ export default function EnhancedTable() {
                     <Table
                         className={classes.table}
                         aria-labelledby="tableTitle"
-                        size={'small'}
-                        aria-label="enhanced table"
+                        size={'medium'}
+                        aria-label="MUItable"
                     >
-                        <EnhancedTableHead
-                            classes={classes}
+                        <MUITableHead/>
 
-                            rowCount={rows.length}
-                        />
                         <TableBody>
-
+            
 
                             {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                                .map((row, index) => {
+                                .map((row) => {
 
-                                    const labelId = `enhanced-table-checkbox-${index}`;
+                    
+                            
+                                  //  const labelId = `table-${index}`;
 
                                     return (
                                         <TableRow
                                             hover
                                             onClick={(event) => handleClick(event, row.name)}
-                                            role="checkbox"
-
+                                            //role="checkbox"
                                             tabIndex={-1}
-                                            key={row.name}
-
-                                        >
-
+                                            key={row.id}
+                                          
+                                            >
+                                        
+{/* 
                                             <TableCell component="th" id={labelId} scope="row" padding="none">
-                                                {row.name}
-                                            </TableCell>
-                                            <TableCell align="right">{row.calories}</TableCell>
-                                            <TableCell align="right">{row.fat}</TableCell>
-                                            <TableCell align="right">{row.carbs}</TableCell>
-                                            <TableCell align="right">{row.protein}</TableCell>
+                                                {row.id}
+                                            </TableCell> */}
+                                            <TableCell align="left" >{row.id}</TableCell>
+                                            <TableCell align="left">{row.time}</TableCell>
+                                            <TableCell align="left">{row.user}</TableCell>
+                                            {/* <TableCell align="right">{row.protein}</TableCell> */}
                                         </TableRow>
                                     );
                                 })}
+
+                            {/* add empty rows for same view */}
                             {emptyRows > 0 && (
-                                <TableRow style={{ height: 33 * emptyRows }}>
+                                <TableRow style={{ height: 52.5 * emptyRows }}>
                                     <TableCell colSpan={6} />
                                 </TableRow>
                             )}
@@ -179,9 +146,6 @@ export default function EnhancedTable() {
 
                 />
             </Paper>
-
-
-
         </div>
     );
 }
